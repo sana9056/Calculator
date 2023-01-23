5bcbbc0522381e22d190d4ff2330f34b03db376d
sana9056 <sana9056@mail.ru>
2023-01-23 21:16:25 +0300

    simple calculator

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+def calculate():
+    number_1 = int(input('Please enter the first number: '))
+    operation = input('Please type in the math operation(+,-,*,/): ')
+    number_2 = int(input('Please enter the second number: '))
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+        again()
+
+
+def again():
+    calc_again = input()
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+
+calculate()
